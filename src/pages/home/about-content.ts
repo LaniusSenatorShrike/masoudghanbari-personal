7163c569e28c06f73ebb2fa723839ecfdb99360b
LaniusSenatorShrike <laniussenatorshrike@gmail.com>
2026-01-12 21:30:25 +0100

    updates the text

--- src/pages/home/about-content.ts
+++ src/pages/home/about-content.ts
@@ -3,9 +3,8 @@ export const aboutContent = {
   bio: {
     title: "",
     paragraphs: [
-      "Hey there! 👋 I'm the guy who gets unreasonably excited about data pipelines. Currently leading the chaos at Marktplaats (yes, the Dutch eBay) where I convince computers to move billions of events without catching fire.",
-      "My superpower? Making Kafka behave and turning 'it works on my machine' into 'it processes 10 million notifications per hour.' AWS and GCP certified, which basically means I've clicked through a lot of cloud consoles and lived to tell the tale.",
-      "When I'm not wrestling with distributed systems or mentoring engineers, you'll find me exploring Amsterdam by bike, breaking things in production (just kidding... mostly), and dreaming about perfectly partitioned data. Let's build something cool together! 🚀"
-    ]
+      "Hey there! Analytics guy here! 👋 \n I currently work at Marktplaats (yes, the Dutch eBay) as a Analytics Engineering Tech Lead \n",
+      "My superpower? I'm always up for the game. I believe playing games helps making great teams and awesome teams build great products. \n",
+      ]
   }
 };
